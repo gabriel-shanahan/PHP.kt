5cb60725bcc5c04af1ac33f143c56d286bff50a6
Gabriel Shanahan <GabrielShanahan@gmail.com>
2019-12-19 14:09:46 +0100

    Created FollowUpContext

---
+++ src/main/kotlin/cz/php/kt/statements/blocks/FollowUpContext.kt
@@ -0,0 +1,18 @@
+package cz.php.kt.statements.blocks
+
+/**
+ * The purpose of [FollowUpContexts][FollowUpContext] is to allow one to define a set of functions that can only be
+ * called in certain situations, e.g. an else block can only follow an if block. For constructs where such restrictions
+ * apply, we define a FollowUpContext which defines the applicable "follow-up" functions, and then return instances of
+ * it whenever necessary.
+ *
+ * @see cz.php.kt.statements.blocks.branching.If
+ */
+interface FollowUpContext {
+    /**
+     * The parent block. This reference is used so we have a way to add children in the DSL functions.
+     *
+     * @see [Block.unaryPlus]
+     */
+    val parent: Block
+}
